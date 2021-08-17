f02248460ac14635aa62a260a9c9098eee3421cd
Yeamin Mahmud <yeamin21@outlook.com>
2021-08-18 01:04:25 +0600

    Update setup.py

--- setup.py
+++ setup.py
@@ -23,7 +23,7 @@ setup(
         "Programming Language :: Python :: 3.6",
         "Programming Language :: Python :: 3.7",
     ],
-    packages=["bangla_stemmer_m"],
+    packages=["bangla_stemmer"],
     include_package_data=True,
     keywords=('bengali', 'bangla', 'stemmer', 'bengali stemmer', 'bangla stemmer')
 )
